92f9865c228ae1323ef09bc4cf7cb5ad4b2d9e2e
Juan122113 <ammiel89@hotmail.com>
2024-08-20 15:19:40 -0300

    Update index.html & styles.css & main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+const dayBtn = document.getElementById("daily");
+const weekBtn = document.getElementById("weekly");
+const monthBtn = document.getElementById("monthly");
+
+const currDivWork = document.getElementById("current-work");
+const pastDivWork = document.getElementById("past-work");
+const currDivPlay = document.getElementById("current-play");
+const pastDivPlay = document.getElementById("past-play");
+const currDivStudy = document.getElementById("current-study");
+const pastDivStudy = document.getElementById("past-study");
+const currDivExercise = document.getElementById("current-exercise");
+const pastDivExercise = document.getElementById("past-exercise");
+const currDivSocial = document.getElementById("current-social");
+const pastDivSocial = document.getElementById("past-social");
+const currDivSelfCare = document.getElementById("current-self-care");
+const pastDivSelfCare = document.getElementById("past-self-care");
+
+const requestURL = "./data.json";
+const request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+request.onload = function () {
+    const times = request.response;
+}
+
+// fetch("./data.json")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
