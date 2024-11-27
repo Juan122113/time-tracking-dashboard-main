e7736b89291a2c70aee555e77618fea10b5a9b4f
Juan122113 <ammiel89@hotmail.com>
2024-11-27 09:13:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -24,15 +24,10 @@ const buttonStyles = ["color", "opacity"];
 const inactiveColor = ["hsl(236, 100%, 87%)", "0.5"];
 const activeColor = ["white", "1"];
 const mouseStates = ['mouseenter', 'mouseleave'];
-// const handleHover = ["handleButtonHover", "handleButtonNoHover"];
+const svgFuncNames = [alEntrar, alSalir];
 
 let times = [];
 let activeButton = null;
-// let inactiveButtons = [];
-
-// console.log(section);
-// console.log(svg);
-console.log("currDivs:", currDivs);
 
 function buttonStylesInactive(buttonStyles, inactiveColor, button, k) {
     button.style[buttonStyles[k]] = inactiveColor[k];
@@ -45,7 +40,6 @@ function buttonStylesActive(buttonStyles, activeColor, button, i) {
 function elseFor(buttonStyles, inactiveColor, button) {
     for (let k = 0; k < buttonStyles.length; k++) {
         buttonStylesInactive(buttonStyles, inactiveColor, button, k);
-        // console.log(inactiveColor[i]);
     }
 }
 
@@ -58,19 +52,9 @@ function ifFor(buttonStyles, activeColor, button) {
 function separatingActiveButtonAndInactive(buttonStyles, activeColor, inactiveColor, button) {
     if (button === activeButton) {
         ifFor(buttonStyles, activeColor, button);
-        // button.style.color = activeColor;
-        // button.style.opacity = "1";
-       
-            // console.log(activeColor[i]);
-        // console.log(activeColor);
     } else {
         elseFor(buttonStyles, inactiveColor, button);
-        // button.style.color = inactiveColor;
-        // button.style.opacity = "0.5";
-        
-        // console.log(inactiveColor);
     }
-    // console.log(activeColor);
 }
 
 function buttonsForEach(buttonStyles, activeColor, inactiveColor, buttons) {
@@ -82,53 +66,7 @@ function buttonsForEach(buttonStyles, activeColor, inactiveColor, buttons) {
 function updateButtonStates(clickedButton, buttonStyles, activeColor, inactiveColor, buttons) {
     activeButton = clickedButton;
     buttonsForEach(buttonStyles, activeColor, inactiveColor, buttons);
-    
-    // console.log(activeColor);
 }
-
-// console.log(activeColor);
-
-// function handleButtonHover(button, mouseState) {
-//     return function() {
-//         if (button !== activeButton) {
-//             // button.style.color = activeColor;
-//             // button.style.opacity = "1";
-//             if (mouseState === 'mouseenter') {
-//                 for (let i = 0; i < buttonStyles.length; i++) {
-//                     button.style[buttonStyles[i]] = activeColor[i];
-//                 }
-//             } else {
-//                 for (let i = 0; i < buttonStyles.length; i++) {
-//                     button.style[buttonStyles[i]] = inactiveColor[i];
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// function handleButtonHover(button) {
-//     return function() {
-//         if (button !== activeButton) {
-//             // button.style.color = activeColor;
-//             // button.style.opacity = "1";
-//             for (let i = 0; i < buttonStyles.length; i++) {
-//                 button.style[buttonStyles[i]] = activeColor[i];
-//             }
-//         }
-//     }
-// }
-
-// function handleButtonNoHover(button) {
-//     return function() {
-//         if (button !== activeButton) {
-//             // button.style.color = inactiveColor;
-//             // button.style.opacity = "0.5";
-//             for (let i = 0; i < buttonStyles.length; i++) {
-//                 button.style[buttonStyles[i]] = inactiveColor[i];
-//             }
-//         }
-//     }
-// }
 
 function handleButtonClick(button, timeframe) {
     return function(e) {
@@ -153,8 +91,6 @@ const validations = {
 function handleButtonHover(button, mouseState, validations) {
     return function() {
         if (button !== activeButton) {
-            // button.style.color = activeColor;
-            // button.style.opacity = "1";
             return validations[mouseState](button);
         }
     }
@@ -163,42 +99,9 @@ function handleButtonHover(button, mouseState, validations) {
 mouseStates.forEach(mouseState => {
     buttons.forEach(button => {
         button.addEventListener(mouseState, handleButtonHover(button, mouseState, validations));
-        // button.addEventListener('mouseleave', handleButtonNoHover(button));
-    })
+    });
 })
 
-// for (let k = 0; k < mouseStates.length; k++) {
-//     buttons.forEach(button => {
-//                     button.addEventListener(mouseStates[k], handleHover[k](button));
-//                     // button.addEventListener('mouseleave', handleButtonNoHover(button));
-//                 })
-// }
-
-// handleHover.forEach(hover => {
-//     mouseStates.forEach(mouseState => {
-//         buttons.forEach(button => {
-//             button.addEventListener(mouseState, hover(button));
-//             // button.addEventListener('mouseleave', handleButtonNoHover(button));
-//         })
-//     });
-// });
-
-// buttons.forEach(button => {
-//     button.addEventListener('mouseenter', handleButtonHover(button));
-//     button.addEventListener('mouseleave', handleButtonNoHover(button));
-// });
-
-// dayBtn.addEventListener("click", handleButtonClick(dayBtn, "daily"));
-// weekBtn.addEventListener("click", handleButtonClick(weekBtn, "weekly"));
-// monthBtn.addEventListener("click", handleButtonClick(monthBtn, "monthly"));
-
-// buttons.forEach(button => {
-//     timeFrames.forEach(timeframe => {
-//         button.addEventListener('click', handleButtonClick(button, timeframe));
-//     })
-// })
-
-// let i = 0;
 
 function assigningPastDivSetAtt(pastDiv) {
     pastDiv.setAttribute("class", "prev");
@@ -207,13 +110,11 @@ function assigningPastDivSetAtt(pastDiv) {
 function assigningPastDivTextCont(timeframe, pastDiv, previous) {
     pastDiv.textContent = `Last ${getPastLabel(timeframe)} - ${previous}hrs`;
     assigningPastDivSetAtt(pastDiv);
-
 }
 
 function assigningCurrDivInnerHTML(timeframe, activity, currDiv, pastDiv, current, previous) {
     currDiv.innerHTML = `<h5>${activity.title}</h5><h1>${current}hrs</h1>`;
-    assigningPastDivTextCont(timeframe, pastDiv, previous);
-            
+    assigningPastDivTextCont(timeframe, pastDiv, previous);      
 }
 
 function assigningCurrentAndPreviousVar(timeframe, activity, currDiv, pastDiv){
@@ -221,33 +122,10 @@ function assigningCurrentAndPreviousVar(timeframe, activity, currDiv, pastDiv){
     assigningCurrDivInnerHTML(timeframe, activity, currDiv, pastDiv, current, previous);
 }
 
-// function assigningPreviousVar(timeframe, activity, currDiv, pastDiv, current) {
-//     const previous = activity.timeframes[timeframe].previous;
-//     assigningCurrDivInnerHTML(timeframe, activity, currDiv, pastDiv, current, previous);
-// }
-
-// function assigningCurrentVar(timeframe, activity, currDiv, pastDiv) {
-//     const current = activity.timeframes[timeframe].current;
-//     assigningPreviousVar(timeframe, activity, currDiv, pastDiv, current);
-            
-// }
-
 function assigningPastDiv(timeframe, activity, index, currDiv) {
     const pastDiv = pastDivs[index];
-    assigningCurrentAndPreviousVar(timeframe, activity, currDiv, pastDiv)
-
-        // if (currDiv && pastDiv) {
-            
-        // }
+    assigningCurrentAndPreviousVar(timeframe, activity, currDiv, pastDiv);
 }
-// function assigningPastDiv(timeframe, activity, index, currDiv) {
-//     const pastDiv = pastDivs[index];
-//     assigningCurrentVar(timeframe, activity, currDiv, pastDiv);
-
-//         // if (currDiv && pastDiv) {
-            
-//         // }
-// }
 
 function assigningCurrDiv(timeframe, activity, index) {
     const currDiv = currDivs[index];
@@ -260,22 +138,6 @@ function updateContent(timeframe, times) {
     });
 }
 
-// function updateContent(timeframe, times) {
-//     times.forEach((activity, index) => {
-//         const currDiv = currDivs[index];
-//         const pastDiv = pastDivs[index];
-
-//         if (currDiv && pastDiv) {
-//             const current = activity.timeframes[timeframe].current;
-//             const previous = activity.timeframes[timeframe].previous;
-
-//             currDiv.innerHTML = `<h5>${activity.title}</h5><h1>${current}hrs</h1>`;
-//             pastDiv.textContent = `Last ${getPastLabel(timeframe)} - ${previous}hrs`;
-//             pastDiv.setAttribute("class", "prev");
-//         }
-//     });
-// }
-
 const getLabel = {
     'daily': 'Day',
     'weekly': 'Week',
@@ -284,25 +146,9 @@ const getLabel = {
 
 function getPastLabel(timeframe) {
     return getLabel[timeframe];
-    // switch(timeframe) {
-    //     case 'daily': return 'Day';
-    //     case 'weekly': return 'Week';
-    //     case 'monthly': return 'Month';
-    //     // default: return '';
-    // }
 }
 
 /// 3 dots hover
-console.log(section);
-
-// function assigningStyleElementoAlEntrar(section, j) {
-//     section[j].style.backgroundColor = "hsl(235, 46%, 20%)";
-// }
-
-// function alEntrar(section) {
-//     for (let j = 0; j < section.length; j++) {
-//     assigningStyleElementoAlEntrar(section, j)};
-// }
 
 function assigningStyleElementoAlEntrar(eleme) {
     eleme.style.backgroundColor = "hsl(235, 46%, 20%)";
@@ -322,31 +168,12 @@ function alSalir() {
         assigningStyleElementoAlSalir(eleme);});
 }
 
-// function alEntrar(section) {
-//     section.forEach(elemento => {
-//         assigningStyleElementoAlEntrar(elemento)});
-// }
-
-// function assigningStyleElementoAlSalir(section, i) {
-//     section[i].style.backgroundColor = "";
-// }
-
-// function alSalir(section) {
-//     for (let i = 0; i < section.length; i++) {
-//     assigningStyleElementoAlSalir(section, i)};
-// }
-
-// function alSalir(section) {
-//     section.forEach(elemento => {
-//     assigningStyleElementoAlSalir(elemento)});
-// }
-
 svg.forEach(eleme => {
-    eleme.addEventListener('mouseenter', alEntrar);
-    eleme.addEventListener('mouseleave', alSalir);
+    for (let r = 0; r < mouseStates.length; r++) {
+        eleme.addEventListener(mouseStates[r], svgFuncNames[i]);
+    }
+    // eleme.addEventListener('mouseleave', alSalir);
 });
-
-// console.log(activeButton);
 
 // Fetch data
 fetch('./data.json')
